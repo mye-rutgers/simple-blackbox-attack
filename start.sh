e2ef36147865ae33daf8f7b0179068ba9b676a49
mye-rutgers <42823376+mye-rutgers@users.noreply.github.com>
2020-10-18 09:24:42 -0400

    Update start.sh for targeted attacks

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ wget http://www.image-net.org/image/tiny/tiny-imagenet-200.zip
 unzip tiny-imagenet-200.zip
 git clone https://github.com/mye-rutgers/simple-blackbox-attack.git
 cd simple-blackbox-attack
-python3 run_simba.py --data_root /app/tiny-imagenet-200 --num_iters 10000 --pixel_attack  --freq_dims 224
+python3 run_simba.py --data_root /app/tiny-imagenet-200 --num_iters 30000 --pixel_attack --targeted --freq_dims 224
